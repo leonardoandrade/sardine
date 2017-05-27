f4b06363b34633be6bbe986690ec08b24be17a80
leonardoandrade <leonardo@andrade.io>
2017-05-27 15:07:06 +0200

    experiments folder

---
+++ sandbox/bitwise_experiments.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+typedef unsigned long long U64;
+
+
+void print_as_binary(U64 theNumber) {
+    char str[73];
+    memset(str, ' ',73);
+    int j=0;
+    for(int i = 0; i<64; i++) {
+        str[j] = theNumber & (int)powl(2, i) ? '1' : '0';
+        if((i+1)%8 == 0) {
+            j++;
+            str[j] = '\n';
+            j++;
+        }
+        else {
+            j++;
+        }
+    }
+    str[j] = '\0';
+    printf("%s\n", str);
+}
+
+int main() {
+    U64 theNumber = 0x0;
+    int banana = 17;
+    int position = 7;
+    theNumber |= banana << position;
+    
+    U64 mask = 0xF << position; // the mask
+    printf("number:\n");
+    print_as_binary(theNumber);
+    printf("mask:\n");
+    print_as_binary(mask);
+
+    int extractedNumber = (theNumber & mask) >> position;
+    printf("extracted number: %d \n", extractedNumber);
+
+
+}
+
